82cec85fefce4ac2786f90d28ea12b7b616999a1
Tanmay Rauth <tanmayrauth123@gmail.com>
2017-11-04 18:48:43 +0530

    Update block.py

--- block.py
+++ block.py
@@ -1,7 +1,7 @@
 import hashlib
 import block_params
 
-
+#individual block 
 class Block():
 
     def __init__(self, params):
@@ -10,7 +10,8 @@ class Block():
         self.timestamp = params.timestamp
         self.data = params.data
         self.hash = self.calc_hash()
-
+    
+    # setting various parameters of block
     def params(self):
         return block_params.BlockParams(
             self.index,
@@ -19,17 +20,21 @@ class Block():
             self.data
         )
 
+    #genesis block is the initial block of our blockchain    
     @classmethod
     def genesis_block(cls):
         params = block_params.BlockParams.genesis_params()
         return cls(params)
 
+    # hash calculating function for a block which defines itergrity and uniqueness
     def calc_hash(self):
         return hashlib.sha256(str(self.params()).encode()).hexdigest()
 
+    #checking previous block
     def has_valid_index(self, previous_block):
         return self.index == previous_block.index + 1
 
+    #maching out previous block hash value 
     def has_valid_previous_hash(self, previous_block):
         return self.previous_hash == previous_block.hash
 
